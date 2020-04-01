a59021f6d2f0214c7248bc082269d6eee448867d
15274972986 <764874087@qq.com>
2020-04-01 09:48:45 +0800

    Origin

---
+++ bin/bpcounter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import pandas as pd
+import numpy as np
+import modCIGAR
+#from pandas import Series
+
+#input filename
+FileName='../inputdir/test'
+
+#read the file
+BpList=pd.read_csv(FileName,sep='\t',header=None)
+BpList.index=range(len(BpList))
+BpList.columns=['Read','Site','CIGAR']
+BpList['RelaBpSite']=''
+
+#take a look at the input file
+#print(BpList.head(10))
+
+r=0
+while r < len(BpList):
+     CIGAR=BpList['CIGAR'][r]
+     #Acoording to CIGAR pattern, calculate relative breakpoint sites
+     chCIGAR=modCIGAR.Split(CIGAR)
+#    print("Row: " + str(r) + " CIGAR: " + CIGAR + ", expected relative breaksite: " + str(chCIGAR))
+     BpList['RelaBpSite'][r]=chCIGAR
+     r+=1
+#take a look at the newly generated column
+#print(BpList.head(10))
+BpList.to_csv('../outputdir/DataforR',sep='\t',header=True)
---
+++ bin/modCIGAR.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+#Filename: modCIGAR.py
+
+import re
+
+def Split(cigar):
+    #Split CIGAR by pattern and remove '' in list, eg:100M50S to ['100','50'] .
+    SplCigarLen=re.split(r'[SHMDI]',cigar)
+    SplCigarLen.remove('')
+#   print(SplCigarLen)
+#   FloatSplCigarLen=list(map(float,SplCigarLen))
+#Convert string into integer as they indicate pattren length.
+    IntSplCigarLen=list(map(int,SplCigarLen))
+    #Split CIGAR to make a list of patterns and remove '', eg:100M50S to ['M','S']
+    SplCigarPat=re.split(r'[0123456789]*',cigar)
+    SplCigarPat.remove('')
+    #Calculate length of the reads.
+    s=0
+    for PatLen in SplCigarLen:
+        s+=int(PatLen)
+#   print(SplCigarPat)
+#   s=IntModcigarLen[0]
+#   e=IntModcigarLen[len(Modcigar)]
+    #Acoording to patterns at both ends, predict possible breakpoint.
+    RelativeBpSite=0
+    if SplCigarPat[0] == "M" and SplCigarPat[len(SplCigarPat)-1] == "M":
+        RelativeBpSite=0
+    elif SplCigarPat[0] != "M" and SplCigarPat[len(SplCigarPat)-1] == "M":
+        RelativeBpSite=-1
+    elif SplCigarPat[0] == "M" and SplCigarPat[len(SplCigarPat)-1] != "M":
+        RelativeBpSite=s-IntSplCigarLen[len(SplCigarLen)-1]
+    elif SplCigarPat[0] != "M" and SplCigarPat[len(SplCigarPat)-1] != "M" and IntSplCigarLen[0] < IntSplCigarLen[len(SplCigarLen)-1]:
+        RelativeBpSite=s-IntSplCigarLen[0]-IntSplCigarLen[len(SplCigarLen)-1]
+    elif SplCigarPat[0] != "M" and SplCigarPat[len(SplCigarPat)-1] != "M" and IntSplCigarLen[0] > IntSplCigarLen[len(SplCigarLen)-1]:
+        RelativeBpSite=-1
+    else:
+        RelativeBpSite=0
+#   String=str(IntSplCigarLen[0]) + SplCigarPat[0] + str(IntSplCigarLen[len(SplCigarLen)-1]) + SplCigarPat[len(SplCigarPat)-1]
+    return RelativeBpSite
